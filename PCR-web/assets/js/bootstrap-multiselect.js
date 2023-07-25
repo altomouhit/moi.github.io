e1f2d7124e23291269eda6d7eef5a8f80c4f3691
altomouhit@gmail.com <altomouhit@gmail.com>
2023-07-25 17:08:24 +0530

    Changes

--- PCR-web/assets/js/bootstrap-multiselect.js
+++ PCR-web/assets/js/bootstrap-multiselect.js
@@ -431,7 +431,7 @@
             nonSelectedText: 'None selected',
             nSelectedText: 'selected',
             allSelectedText: 'All selected',
-            numberDisplayed: 3,
+            numberDisplayed: 1,
             disableIfEmpty: false,
             disabledText: '',
             delimiterText: ', ',
