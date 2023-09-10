eedb494ff96cb2a4ea478e2a168067dd2100306f
altomouhit@gmail.com <altomouhit@gmail.com>
2023-09-10 10:53:09 +0530

    Changes

---
+++ PCR-web/assets/js/PCR_incidents.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+    function format(data) {
+        // `d` is the original data object for the row
+        return '<div class="accordion custom-accordionwithicon accordion-fill-info" id="accordionFill">'+
+        '<div class="accordion-item">'+
+            '<h2 class="accordion-header" id="accordionFillExample2">'+
+                '<button class="accordion-button fw-semibold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accor_fill2" aria-expanded="false" aria-controls="accor_fill2">'+
+                    data.stationName +
+                '</button>'+
+            '</h2>'+
+            '<div id="accor_fill2" class="accordion-collapse collapse" aria-labelledby="accordionFillExample2" data-bs-parent="#accordionFill">'+
+                '<div class="accordion-body">'+
+                '<table class="table align-middle table-nowrap mb-0 table-bordered table-striped expandable-table w-100" id="PCRIncidentsInsideTable">'+
+                        '<thead class="table-light">'+
+                            '<tr>'+
+                                '<th>PCR</th>'+
+                                '<th>Name</th>'+
+                                '<th class="w-5"></th>'+
+                            '</tr>'+
+                        '</thead>'+
+                        '<tbody>'+
+                            '<tr>'+
+                                '<td>' + data.PCR + '</td>'+
+                                '<td class="">' + data.PCRName + '</td>'+
+                                '<td>'+
+                                    '<ul class="list-inline hstack gap-2 mb-0">'+
+                                        '<li class="list-inline-item" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" title="Edit">'+
+                                            '<a class="edit-item-btn" href="#showModal" data-bs-toggle="modal"><i class="ri-pencil-fill align-bottom text-white btn btn-info"></i></a>'+
+                                        '</li>'+
+                                    '</ul>'+
+                                '</td>'+
+                            '</tr>'+
+                        '</tbody>'+
+                    '</table>'+
+                '</div>'+
+            '</div>'+
+        '</div>'+
+    '</div>';
+    }
+    $('#PCRIncidentsTable').DataTable({
+        "ajax": "assets/js/json/data.json",
+        "columns": [
+            { "data": "Quote" },
+            { "data": "incidentLocation" },
+            { "data": "stationName" },
+            { "data": "Date" },
+            { "data": "callType" },
+            { "data": "subCategory" },
+            { "data": null, "className": 'details-control', "orderable": false, "defaultContent": '' }],
+        "order": [[1, 'asc']],
+        "destroy": true,
+        "dom": '<"top"f>rt<"bottom"ilp>',
+        //"paging": false,
+        //"ordering": false,
+        //"info": false,
+        //"filter": false,
+        columnDefs: [{
+            orderable: false,
+            className: 'select-checkbox',
+            targets: 0
+        }],
+        select: {
+            style: 'os',
+            selector: 'td:first-child'
+        }
+    });
+    $('#PCRIncidentsTable tbody').on('click', 'td.details-control', function() {
+        var table = $('#PCRIncidentsTable').DataTable();
+        var tr = $(this).parents('tr');
+        var data = table.row($(this).parents('tr'));
+        if (data.child.isShown()) {
+            // This row is already open - close it
+            data.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            
+            table.rows().every(function(){
+            // If row has details expanded
+            if(this.child.isShown()){
+                // Collapse row details
+                this.child.hide();
+                $(this.node()).removeClass('shown');
+            }
+        });
+        
+            // Open this row
+            data.child(format(data.data())).show();
+            tr.addClass('shown');
+        }
+    });
+
+    $('#unreadPCRTable').DataTable({
+        processing: true,
+        //serverSide: true,
+        "destroy": true,
+        "dom": '<"top"f>rt<"bottom"ilp>',
+        "order": [[1, 'asc']],
+    });
+});
---
+++ PCR-web/assets/js/mailbox.js
@@ -0,0 +1,150 @@
+var url = "assets/js/json/",
+	allmaillist = "";
+const loader = document.querySelector("#elmLoader");
+var getJSON = function(e, t) {
+	var a = new XMLHttpRequest;
+	a.open("GET", url + e, !0), a.responseType = "json", a.onload = function() {
+		var e = a.status;
+		200 === e ? (document.getElementById("elmLoader").innerHTML = "", t(null, a.response)) : t(e, a.response)
+	}, a.send()
+};
+
+function loadMailData(e) {
+	 Array.from(e).forEach(function(e, t) {
+		var a = e.readed ? "" : "unread",
+			l = e.starred ? "active" : "",
+			c = e.counted ? "(" + e.counted + ")" : "";
+		document.querySelector("#mail-list").innerHTML +=
+		'<tr class="' + a + '">'+
+		'<td class="">'+
+			'<a href="javascript: void(0);"><span class="title-name">' + e.incidentNo +' </span>' + c + '</a>'+
+		'</td>'+
+		'<td>'+
+			'<a href="javascript: void(0);"><span class="title-date">' + e.Date +' </span></a>'+
+		'<td>'+
+			'<a href="javascript: void(0);"><span class="title-callType">' + e.callType +' </span></a>'+
+		'</td>'+
+		'<td><a href="javascript: void(0);" class="subject">'+
+			'<span class="subject-title">' + e.subCategory + '</span></a>'+
+		'</td>'+
+		'<td><a href="javascript: void(0);" class="subject"><span class="teaser">' + e.stationName + '</span></a></td>'+
+		'<td class="col-mail col-mail">'+
+			'<div class="date">' + e.incidentLocation + '</div>'+
+		'</td>'+
+	'</tr>',
+        emailDetailShow(), emailDetailChange()
+	})
+}
+
+function favouriteBtn() {
+	Array.from(document.querySelectorAll(".favourite-btn")).forEach(function(e) {
+		e.addEventListener("click", function() {
+			e.classList.contains("active") ? e.classList.remove("active") : e.classList.add("active")
+		})
+	})
+}
+
+function emailDetailShow() {
+	var a = document.getElementsByTagName("body")[0],
+		t = (Array.from(document.querySelectorAll(".message-list a")).forEach(function(e) {
+			e.addEventListener("click", function(t) {
+				a.classList.add("email-detail-show"), Array.from(document.querySelectorAll(".message-list tr.unread")).forEach(function(e) {
+					e.classList.contains("unread") && t.target.closest("tr").classList.remove("unread")
+				})
+			})
+		}), Array.from(document.querySelectorAll(".close-btn-email")).forEach(function(e) {
+			e.addEventListener("click", function() {
+				a.classList.remove("email-detail-show")
+			})
+		}), !1),
+		l = document.getElementsByClassName("email-menu-sidebar");
+	Array.from(document.querySelectorAll(".email-menu-btn")).forEach(function(e) {
+		e.addEventListener("click", function() {
+			Array.from(l).forEach(function(e) {
+				e.classList.add("menubar-show"), t = !0
+			})
+		})
+	}), 
+    favouriteBtn()
+}
+
+
+getJSON("mail-list.init.json", function(e, t) {
+	null !== e ? console.log("Something went wrong: " + e) :
+	 (allmaillist = t[0].primary, 
+		loadMailData(allmaillist))
+}), Array.from(document.querySelectorAll(".mail-list a")).forEach(function(l) {
+	l.addEventListener("click", function() {
+		var t, e, a = document.querySelector(".mail-list a.active");
+		a && a.classList.remove("active"), l.classList.add("active"), l.querySelector(".mail-list-link").hasAttribute("data-type") ? (t = l.querySelector(".mail-list-link").innerHTML, e = allmaillist.filter(e => e.labeltype === t)) : (t = l.querySelector(".mail-list-link").innerHTML, document.getElementById("mail-list").innerHTML = "", e = "All" != t ? allmaillist.filter(e => e.tabtype === t) : allmaillist, document.getElementById("mail-filter-navlist").style.display = "All" != t && "Inbox" != t ? "none" : "block"), loadMailData(e), favouriteBtn()
+	})
+});
+
+function removeItems() {
+	document.getElementById("removeItemModal").addEventListener("show.bs.modal", function(e) {
+		document.getElementById("delete-record").addEventListener("click", function() {
+			Array.from(document.querySelectorAll(".message-list li")).forEach(function(e) {
+				var t, a;
+				e.classList.contains("active") && (t = e.querySelector(".form-check-input").value, a = t, allmaillist = allmaillist.filter(function(e) {
+					return e.id != a
+				}), e.remove())
+			}), document.getElementById("btn-close").click(), document.getElementById("email-topbar-actions") && (document.getElementById("email-topbar-actions").style.display = "none"), checkall.indeterminate = !1, checkall.checked = !1
+		})
+	})
+}
+
+function removeSingleItem() {
+	var a;
+	document.querySelectorAll(".remove-mail").forEach(function(t) {
+		t.addEventListener("click", function(e) {
+			a = t.getAttribute("data-remove-id"), document.getElementById("delete-record").addEventListener("click", function() {
+				var t;
+				t = a, loadMailData(allmaillist = allmaillist.filter(function(e) {
+					return e.id != t
+				})), document.getElementById("close-btn-email").click()
+			})
+		})
+	})
+}
+
+mailChatDetailElm = !1;
+
+function emailDetailChange() {
+	Array.from(document.querySelectorAll(".message-list tr")).forEach(function(c) {
+		c.addEventListener("click", function() {
+			var e = (document.querySelector(".remove-mail").setAttribute("data-remove-id", e), 
+				c.querySelector(".title-name").innerHTML),
+				a = (document.querySelector(".email-subject-title").innerHTML = e)
+		})
+	})
+}
+document.querySelectorAll(".email-chat-list a").forEach(function(l) {
+	var e, t;
+	l.classList.contains("active") && (document.getElementById("emailchat-detailElem").style.display = "block", e = document.querySelector(".email-chat-list a.active").querySelector(".chatlist-user-name").innerHTML, t = document.querySelector(".email-chat-list a.active").querySelector(".chatlist-user-image img").getAttribute("src"), document.querySelector(".email-chat-detail .profile-username").innerHTML = e, document.getElementById("users-conversation").querySelectorAll(".left .chat-avatar").forEach(function(e) {
+		
+	})), l.addEventListener("click", function(e) {
+		document.getElementById("emailchat-detailElem").style.display = "block", mailChatDetailElm = !0;
+		var t = document.querySelector(".email-chat-list a.active"),
+			t = (t && t.classList.remove("active"), this.classList.add("active"), scrollToBottom("users-chat"), l.querySelector(".chatlist-user-name").innerHTML),
+			a = l.querySelector(".chatlist-user-image img").getAttribute("src"),
+			t = (document.querySelector(".email-chat-detail .profile-username").innerHTML = t, document.getElementById("users-conversation"));
+		Array.from(t.querySelectorAll(".left .chat-avatar")).forEach(function(e) {
+			a ? e.querySelector("img").setAttribute("src", a) : e.querySelector("img").setAttribute("src", dummyUserImage)
+		})
+	})
+});
+
+const triggerTabList = document.querySelectorAll("#mail-filter-navlist .nav-tabs button");
+
+function resizeEvent() {
+	var e;
+	document.documentElement.clientWidth < 767 && ((e = document.querySelector(".email-chat-list a.active")) && e.classList.remove("active"), document.getElementById("emailchat-detailElem").style.display = "none")
+}
+triggerTabList.forEach(e => {
+	const t = new bootstrap.Tab(e);
+	e.addEventListener("click", e => {
+		e.preventDefault();
+		document.querySelector(".tab-content .tab-pane.show");
+		t.show()
+	})
+}), resizeEvent(), window.onresize = resizeEvent;
